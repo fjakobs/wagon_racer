ddc90a3315ef9f84f70fcb9a5b9519a6a2b7a47c
Fabian Jakobs <fabian.jakobs@web.de>
2011-03-20 17:52:59 +0000

    first 1k version!

--- racer.js
+++ racer.js
@@ -3,134 +3,82 @@
 var C40 = 40
 var C150 = 150
 var CFillStyle = "fillStyle"
-var CStrokeStyle = "strokeStyle"
-var CAddColorStop = "addColorStop"
+//var CAddColorStop = "addColorStop"
 var CLineWidth = "lineWidth"
 var CHsl27 = "hsl(27,"
 var Ma = Math
 
-
-//var short = {}
-//for (var k in a) {
-//    var nk = k[0]+k.replace(/[^A-Z]/g, "")
-//    if (short[nk])
-//        console.log("collision", k, short[nk])
-//    short[nk] = k
-//}
-//    
 for(k in a)
     a[k[0]+k.replace(/[^A-Z]/g,"")]=a[k]
 
     
-M = []
-m(0, C40, 600, C40, 8)
-M.push(500, C150, 0, C150)
-M1 = M
-M = []
-m(0, 50, 500, 50, 8)
-M.push(500, C150, 0, C150)
-
-function m(x1, y1, x2, y2, n) {    
-    if (!n)
-        return M.push(x2, y2)
-    var d = (x2-x1)/4
-    var y = (y1 + y2) / 2 + Ma.random() * d - d/ 2
-    m(x1, y1, x1+2*d, y, n-1)
-    m(x1+2*d, y, x2, y2, n-1)
+M = [99]
+m(50, 50, 512)
+M[512] = 99
+function m(y1, y2, l) {    
+    if (l<=1)
+        return M.push(y1)
+        
+    var l2 = l/2
+    var y = (y1+y2+Ma.random()*l2-l/4) / 2
+    m(y1, y, l/2)
+    m(y, y2, l/2)
 }
-/*
-function wagon(x) {
-    a[CFillStyle] = "#ead2b6"
-    a[CStrokeStyle] = "#7c6c4e"
-    a[CLineWidth] = 2
-    a.bP()
-    for (i=0; i<6; a.a(C150, 110+i, 21-i, 2, 8), i+=2);
-    a.f()
-    a.s()
-    
-    a.bP()
-    a[CFillStyle] = "#ddbc75"
-    a[CStrokeStyle] = "#44260b"
-    a.moveTo(132, 119)
-    a.lT(168, 119)
-    a.lT(164, 135)
-    a.lT(136, 135)
-    a.closePath()
-    a.f()
-    a.s()
 
-    a[CLineWidth] = 4
-    a.bP()
-    a.lT(129+Math.sin(x), 122)
-    a.lT(132-Math.sin(x), 143)
-    
-    a.moveTo(171+Math.sin(x), 122)
-    a.lT(168-Math.sin(x), 143)
-    a.s()
-}*/
-
-function doIt() {
+setInterval(function() {
     var tick = new Date() / 10;
     
     // sky
-    var sky = a.cLG(0, 0, 0, C150)
-    sky[CAddColorStop](0, '#25b')
-    sky[CAddColorStop](1, '#8bd')
-    a[CFillStyle] = sky
+//    var sky = a.cLG(0, 0, 0, C150)
+//    sky[CAddColorStop](0, "#25b")
+//    sky[CAddColorStop](1, "#8bd")
+//    a[CFillStyle] = sky
+    a[CFillStyle] = "#8bd"
     a.fR(0, 0, 300, 75)
     
     // mountain
     var x = Ma.cos(tick/C40)*C40
+    
     a[CFillStyle] = "#555"
-    a.bP()
-    a.mT(0, C40)
-    for (i=0; i<M.length; a.lT(M1[i++]-C150+x*0.8, M1[i++]));
+    a.bP()    
+    for (i=0; i<513; a.lT(i-C150+x*0.8, M[512-i++]-10));
     a.f()
     
     a[CFillStyle] = "#333"
-    a.bP()
-    a.mT(0, 50)
-    for (i=0; i<M.length; a.lT(M[i++]-C150+x, M[i++]));
+    a.bP()    
+    for (i=0; i<513; a.lT(i-C150+x, M[i++]));
     a.f()
     
-    var offset = tick%C40
-    var e = 90
-    var h = 0
-    var scale = 4
-    for (var y=C150; y>=75; y--) {
-        var d = h*e/(e-h)        
+    for (h=0; h<=75; h++) {
+        var d = h*90/(90-h)        
         var shade = d/450
-        var colors = (offset + d) % C40 < 20 
+        var colors = (tick + d) % C40 < 20 
             ? [CHsl27+(C40-shade*C40)+"%,52%)", CHsl27+(73-shade*73)+"%,23%)"] 
             : [CHsl27+(C40-shade*C40)+"%,54%)", CHsl27+(C40-shade*C40)+"%,41%)"]
-        colors[2] = CHsl27+(C40-shade*C40)+"%,33%)"
-        
+
         var shift = Ma.sin((h+tick)/ C40)*30
-        
+        y = C150-h
         a[CFillStyle] = colors[0]
         a.fR(0, y, 300, 1)
-        a[CFillStyle] = colors[2]
+        var scale = 4-0.05*h
+        a[CFillStyle] = CHsl27+(C40-shade*C40)+"%,33%)"
         a.fR(-35*scale+shift+C150, y, 70*scale, 1)
         a[CFillStyle] = colors[1]
         a.fR(-30*scale+shift+C150, y, 60*scale, 1)
-        
-        h++
-        scale -= 0.048
     }
     
     x = Ma.sin(tick/15)
     // wagon
-    a[CFillStyle] = "#ead2b6"
-    a[CStrokeStyle] = "#7c6c4e"
-    a[CLineWidth] = 2
+    a[CFillStyle] = "#dca"
+    a.strokeStyle = "#421"
+    a[CLineWidth] = 1
     a.bP()
     for (i=0; i<6; a.a(C150, 110+i, 21-i, 2, 8), i+=2);
     a.f()
     a.s()
     
     a.bP()
-    a[CFillStyle] = "#ddbc75"
-    a[CStrokeStyle] = "#44260b"
+    a[CFillStyle] = "#db7"
     a.moveTo(132, 119)
     a.lT(168, 119)
     a.lT(164, 135)
@@ -147,8 +95,6 @@ function doIt() {
     a.moveTo(171+x, 122)
     a.lT(168-x, 143)
     a.s()
-};
-
-setInterval(doIt, 10)
+}, 0);
 
 })();
---
+++ racer_min.js
@@ -0,0 +1,74 @@
+function l(a, b, c) {
+    if (c <= 1) return M.push(a);
+    var d = c / 2,
+    e = (a + b + j.random() * d - c / 4) / 2;
+    l(a, e, c / 2),
+    l(e, b, c / 2)
+}
+b = 40,
+c = 150,
+d = "fillStyle",
+e = "lineWidth",
+g = "hsl(27,",
+j = Math;
+for (k in a) a[k[0] + k.replace(/[^A-Z]/g, "")] = a[k];
+M = [99],
+l(50, 50, 512),
+M[512] = 99,
+setInterval(function() {
+    f = new Date / 10;
+    a[d] = "#8bd",
+    a.fR(0, 0, 300, 75);
+    k = j.cos(f / b) * b;
+    a[d] = "#555",
+    a.bP();
+    for (i = 0; i < 513; a.lT(i - c + k * .8, M[512 - i++] - 10));
+    a.f(),
+    a[d] = "#333",
+    a.bP();
+    for (i = 0; i < 513; a.lT(i - c + k, M[i++]));
+    a.f();
+    for (h = 0; h <= 75; h++) {
+        l = h * 90 / (90 - h),
+        m = l / 450,
+        n = (f + l) % b < 20 ? [g + (b - m * b) + "%,52%)", g + (73 - m * 73) + "%,23%)"] : [g + (b - m * b) + "%,54%)", g + (b - m * b) + "%,41%)"],
+        o = j.sin((h + f) / b) * 30;
+        y = c - h,
+        a[d] = n[0],
+        a.fR(0, y, 300, 1);
+        p = 4 - .05 * h;
+        a[d] = g + (b - m * b) + "%,33%)",
+        a.fR( - 35 * p + o + c, y, 70 * p, 1),
+        a[d] = n[1],
+        a.fR( - 30 * p + o + c, y, 60 * p, 1)
+    }
+    k = j.sin(f / 15)
+    
+    A = a
+    with(A) {
+        A[d] = "#dca",
+        strokeStyle = "#421",
+        A[e] = 1,
+        bP();
+        for (i = 0; i < 6; a(150, 110 + i, 21 - i, 2, 8), i += 2);
+        f(),
+        s(),
+        bP(),
+        A[d] = "#db7",
+        moveTo(132, 119),
+        lT(168, 119),
+        lT(164, 135),
+        lT(136, 135),
+        closePath(),
+        f(),
+        s(),
+        A[e] = 4,
+        bP(),
+        lT(129 + k, 122),
+        lT(132 - k, 143),
+        moveTo(171 + k, 122),
+        lT(168 - k, 143),
+        s()
+    }
+},
+0)
